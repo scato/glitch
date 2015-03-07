6409198f6c641b991ebf5b5b2a437857ef6bd8ac
Scato Eggen <seggen@ibuildings.nl>
2015-03-07 22:15:52 +0100

    Implement EventValue

--- spec/Glitch/Interpreter/EventValueSpec.php
+++ spec/Glitch/Interpreter/EventValueSpec.php
@@ -2,13 +2,31 @@
 
 namespace spec\Glitch\Interpreter;
 
+use Glitch\Interpreter\EventValue;
+use Glitch\Interpreter\StringValue;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class EventValueSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_fires_listeners_that_were_added(EventValue $listener)
     {
-        $this->shouldHaveType('Glitch\Interpreter\EventValue');
+        $value = new StringValue('test');
+
+        $listener->fire($value)->shouldBeCalled();
+
+        $this->add($listener);
+        $this->fire($value);
+    }
+
+    function it_will_not_fire_listeners_that_were_removed(EventValue $listener)
+    {
+        $value = new StringValue('test');
+
+        $listener->fire($value)->shouldNotBeCalled();
+
+        $this->add($listener);
+        $this->remove($listener);
+        $this->fire($value);
     }
 }
--- src/Glitch/Interpreter/EventValue.php
+++ src/Glitch/Interpreter/EventValue.php
@@ -4,4 +4,30 @@ namespace Glitch\Interpreter;
 
 class EventValue
 {
+    private $listeners = array();
+
+    public function add(EventValue $listener)
+    {
+        $this->listeners[] = $listener;
+    }
+
+    public function remove(EventValue $listener)
+    {
+        $listeners = array();
+
+        foreach ($this->listeners as $listener) {
+            if ($listener !== $listener) {
+                $listeners[] = $listener;
+            }
+        }
+
+        $this->listeners = $listeners;
+    }
+
+    public function fire($value)
+    {
+        foreach ($this->listeners as $listener) {
+            $listener->fire($value);
+        }
+    }
 }
