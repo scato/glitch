a777d24ed896804d4fa14eea3f97b9e4203b2bdc
Scato Eggen <seggen@ibuildings.nl>
2015-03-08 16:23:36 +0100

    Implement ActionValue

--- spec/Glitch/Interpreter/ActionValueSpec.php
+++ spec/Glitch/Interpreter/ActionValueSpec.php
@@ -2,13 +2,31 @@
 
 namespace spec\Glitch\Interpreter;
 
+use Glitch\Grammar\Tree\StatementNode;
+use Glitch\Interpreter\ActivationObject;
+use Glitch\Interpreter\StringValue;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class ActionValueSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function let(StatementNode $statement, ActivationObject $parentScope)
     {
-        $this->shouldHaveType('Glitch\Interpreter\ActionValue');
+        $this->beConstructedWith(['x'], [$statement], $parentScope);
+    }
+    
+    function it_is_an_event()
+    {
+        $this->shouldHaveType('Glitch\Interpreter\EventValue');
+    }
+
+    function it_should_invoke_its_statements_when_fired(StatementNode $statement, ActivationObject $parentScope, StringValue $value)
+    {
+        $this->fire($value);
+
+        $scope = new ActivationObject($parentScope->getWrappedObject());
+        $scope->set('x', $value->getWrappedObject());
+
+        $statement->invoke($scope)->shouldBeCalled();
     }
 }
--- src/Glitch/Interpreter/ActionValue.php
+++ src/Glitch/Interpreter/ActionValue.php
@@ -2,6 +2,26 @@
 
 namespace Glitch\Interpreter;
 
-class ActionValue
+class ActionValue extends EventValue
 {
+    private $parameters;
+    private $statements;
+    private $parentScope;
+
+    public function __construct(array $parameters, array $statements, ActivationObject $parentScope)
+    {
+        $this->parameters = $parameters;
+        $this->statements = $statements;
+        $this->parentScope = $parentScope;
+    }
+
+    public function fire($value)
+    {
+        $scope = new ActivationObject($this->parentScope);
+        $scope->set($this->parameters[0], $value);
+
+        foreach ($this->statements as $statement) {
+            $statement->invoke($scope);
+        }
+    }
 }
