d2eece2e30fcc3cd778463aca61fe5069547c57b
Scato Eggen <seggen@ibuildings.nl>
2015-03-26 07:37:04 +0100

    Add BinaryNode with +, -, ===, !==, < and >

---
+++ spec/Glitch/Grammar/Tree/BinaryNodeSpec.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace spec\Glitch\Grammar\Tree;
+
+use Glitch\Grammar\Tree\ExpressionNode;
+use Glitch\Runtime\ActivationObject;
+use Glitch\Runtime\StringValue;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class BinaryNodeSpec extends ObjectBehavior
+{
+    function it_is_an_expression(ExpressionNode $left, ExpressionNode $right)
+    {
+        $this->beConstructedWith('+', $left, $right);
+
+        $this->shouldHaveType('Glitch\Grammar\Tree\ExpressionNode');
+    }
+
+    function it_should_add_two_numbers(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('+', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('1'));
+        $right->reduce($scope)->willReturn(new StringValue('2'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('3'));
+    }
+
+    function it_should_subtract_two_numbers(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('-', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('1'));
+        $right->reduce($scope)->willReturn(new StringValue('2'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('-1'));
+    }
+
+    function it_should_see_that_two_strings_are_equal(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('===', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('foo'));
+        $right->reduce($scope)->willReturn(new StringValue('foo'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('true'));
+    }
+
+    function it_should_see_that_two_strings_are_not_equal(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('===', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('foo'));
+        $right->reduce($scope)->willReturn(new StringValue('bar'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('false'));
+    }
+
+    function it_should_see_that_two_strings_are_not_unequal(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('!==', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('foo'));
+        $right->reduce($scope)->willReturn(new StringValue('foo'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('false'));
+    }
+
+    function it_should_see_that_two_strings_are_unequal(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('!==', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('foo'));
+        $right->reduce($scope)->willReturn(new StringValue('bar'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('true'));
+    }
+
+    function it_should_see_that_one_string_comes_before_another(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('<', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('bar'));
+        $right->reduce($scope)->willReturn(new StringValue('foo'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('true'));
+    }
+
+    function it_should_see_that_one_string_does_not_come_before_another(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('<', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('foo'));
+        $right->reduce($scope)->willReturn(new StringValue('bar'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('false'));
+    }
+
+    function it_should_see_that_one_number_comes_before_another(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('<', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('9'));
+        $right->reduce($scope)->willReturn(new StringValue('10'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('true'));
+    }
+
+    function it_should_see_that_one_string_comes_after_another(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('>', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('foo'));
+        $right->reduce($scope)->willReturn(new StringValue('bar'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('true'));
+    }
+
+    function it_should_see_that_one_string_does_not_come_after_another(ExpressionNode $left, ExpressionNode $right, ActivationObject $scope)
+    {
+        $this->beConstructedWith('>', $left, $right);
+        $left->reduce($scope)->willReturn(new StringValue('bar'));
+        $right->reduce($scope)->willReturn(new StringValue('foo'));
+
+        $this->reduce($scope)->shouldBeLike(new StringValue('false'));
+    }
+}
---
+++ src/Glitch/Grammar/Tree/BinaryNode.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Glitch\Grammar\Tree;
+
+use Glitch\Runtime\ActivationObject;
+use Glitch\Runtime\StringValue;
+use RuntimeException;
+
+class BinaryNode implements ExpressionNode
+{
+    private $operator;
+    private $left;
+    private $right;
+
+    public function __construct($operator, ExpressionNode $left, ExpressionNode $right)
+    {
+        $this->operator = $operator;
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    private function apply($left, $right)
+    {
+        switch ($this->operator) {
+            case '+':
+                return $left->toString() + $right->toString();
+            case '-':
+                return $left->toString() - $right->toString();
+            case '===':
+                return $left->toString() === $right->toString();
+            case '!==':
+                return $left->toString() !== $right->toString();
+            case '<':
+                return strnatcmp($left->toString(), $right->toString()) < 0;
+            case '>':
+                return strnatcmp($left->toString(), $right->toString()) > 0;
+            default:
+                throw new RuntimeException("Operator '{$this->operator}' not supported");
+        }
+    }
+
+    public function reduce(ActivationObject $scope)
+    {
+        $left = $this->left->reduce($scope);
+        $right = $this->right->reduce($scope);
+
+        $value = $this->apply($left, $right);
+
+        if (is_bool($value)) {
+            $value = $value ? 'true' : 'false';
+        }
+
+        return new StringValue($value);
+    }
+}
